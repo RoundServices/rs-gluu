4963b32e75e8d82062e88c727bf954b5107a9c38
ggallard <ggallard@identicum.com>
2021-10-20 08:53:58 -0300

    First commit, split from rs-partners

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rs-gluu
+
+## Pre-requisites
+- Python > 3.x
+- [rs-utils](https://github.com/RoundServices/rs-utils)
+
+## Deploy
+```sh
+python3 -m pip install --upgrade git+https://github.com/RoundServices/rs-gluu.git@main
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+# rs-midpoint is available under the MIT License. https://github.com/roundservices/rs-midpoint/
+# Copyright (c) 2021, Round Services LLC - https://roundservices.biz/
+#
+# Author: Gustavo J Gallardo - ggallard@roundservices.biz
+#
+
+from setuptools import setup
+
+setup(
+    name='rs-gluu',
+    version='1.0.0',
+    description='Python utilities for Gluu',
+    url='git@github.com:RoundServices/rs-gluu.git',
+    author='Round Services',
+    author_email='ggallard@roundservices.biz',
+    license='MIT License',
+    install_requires=['python-ldap', 'ldif', 'pyDes', 'rs-utils'],
+    packages=['rs.gluu'],
+    zip_safe=False,
+    python_requires='>=3.0'
+)
